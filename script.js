ab51d32a4c5c840120843da8c14e3e2152a41ca7
rzndluisa <rzndluisa@gmail.com>
2024-09-18 10:26:47 -0300

    movimento da bolinha

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 //Pegando o círculo e a área de jogo
-const circle = document. getElementById('circle');
-const gameArea = document. getElementById('gameArea');
+const circle = document.getElementById('circle');
+const gameArea = document.getElementById('gameArea');
+
+let posX = 50;
+let posY = 50;
+
+const moveSpeed = 10;
+const originalBackgroundColor = 'lightskyblue';
+
+// Função que move o círculo
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'ArrowUp':
+            if (posY > 0)
+                posY = posY - moveSpeed;
+            break;
+        case 'ArrowDown':
+            if (posY < window.innerHeight - circle.offsetHeight) posY += moveSpeed;
+            break;
+        case 'ArrowLeft':
+            if (posX > 0) posX -= moveSpeed:
+            break;
+        case 'ArrowRight':
+            if (posX < window.innerWidth - circle.offsetWidth) posX += moveSpeed;
+            break;
+    }
+    updateCirclePosition();
+});
