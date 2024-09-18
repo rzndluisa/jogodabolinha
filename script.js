579e9829a0e63d1c9e3594c7d3ba271bf708e765
rzndluisa <rzndluisa@gmail.com>
2024-09-18 11:06:22 -0300

    criação do quadrado

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ function updateCirclePosition() {
     circle.style.left = `${posX}px`;
     circle.style.top = `${posY}px`;
 }
+
+//Função para criar quadrados
+function createSquares() {
+    clearShapes();
+    gameArea.style.backgroundColor = 'violet';
+
+    for (let i = 0; i < 4; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.left = `${´Math.random() * (window.innerWidth - 50)} px`;
+    square.style.top = `${ Math.random() * (window.innerHeight - 50)} px`;
+    gameArea.appendChild(square);
+}
+}
